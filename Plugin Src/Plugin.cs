b132a7115996565141ec27d7ce1fefdb1178990f
Cat <104047478+WasntCat@users.noreply.github.com>
2023-08-16 21:14:22 -0400

    Plugin Base

---
+++ Plugin Src/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using KI.obfuscation.IPlugin;
+using System;
+
+namespace KI_Plugin_Template
+{
+    public class Plugin : IPluginLoader
+    {
+        public void Execute(ModuleDefMD module)
+        {
+        
+        }
+    }
+}
